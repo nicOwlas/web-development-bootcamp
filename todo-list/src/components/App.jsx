4788be8c040efe201b2a6105f9e043465e6ad238
Nicolas Draber <nicolas.draber@gmail.com>
2023-06-10 15:12:29 +0200

    working

--- todo-list/src/components/App.jsx
+++ todo-list/src/components/App.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [itemList, setItemList] = useState([]);
+
+  function handleInput(event) {
+    const newInput = event.target.value;
+    setInputText(newInput);
+  }
+
+  function handleAdd(event) {
+    inputText !== "" &&
+      setItemList((previousValue) => [inputText, ...previousValue]);
+    setInputText("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleInput} value={inputText} type="text" />
+        <button onClick={handleAdd}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>An Item</li>
+          {itemList.map((item) => (
+            <li>{item}</li>
+          ))}
         </ul>
       </div>
     </div>
